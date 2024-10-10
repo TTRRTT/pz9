c3d9c183c47bd0a301c6883845d209de49c2e1bf
TTRRTT <artem.natharov@gmail.com>
2024-10-10 15:30:36 +0700

    init commit

---
+++ app/src/main/java/com/example/practika9/LinearLayouts.java
@@ -0,0 +1,35 @@
+package com.example.practika9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LinearLayouts extends AppCompatActivity implements View.OnClickListener {
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.linear_layouts);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn1 = findViewById(R.id.button2);
+        btn1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, TableLayouts.class));
+    }
+}
